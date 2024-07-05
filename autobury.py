4316d67029ec48429e9f569273c5f0ca969e7fc9
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-05 17:33:56 +0200

    use my own wrapper

--- autobury.py
+++ autobury.py
@@ -1,35 +1,9 @@
 import urllib
 import urllib.request
 import json
+from py_ankiconnect import PyAnkiconnect
 
-def ankiconnect(action, **params):
-    """
-    used to send request to anki using the addon anki-connect
-    """
-    def request_wrapper(action, **params):
-        return {'action': action, 'params': params, 'version': 6}
-
-    requestJson = json.dumps(request_wrapper(action, **params)
-                             ).encode('utf-8')
-    try:
-        response = json.load(urllib.request.urlopen(
-            urllib.request.Request(
-                'http://localhost:8775',
-                requestJson)))
-    except (ConnectionRefusedError, urllib.error.URLError) as e:
-        # Anki is probably not running, exiting without print
-        raise SystemExit()
-
-    if len(response) != 2:
-        raise Exception('response has an unexpected number of fields')
-    if 'error' not in response:
-        raise Exception('response is missing required error field')
-    if 'result' not in response:
-        raise Exception('response is missing required result field')
-    if response['error'] is not None:
-        raise Exception(response['error'])
-    return response['result']
-
+ankiconnect = PyAnkiconnect()
 
 not_buried = ankiconnect(action="findCards", query="added:1 is:new -is:buried")
 if not_buried:
